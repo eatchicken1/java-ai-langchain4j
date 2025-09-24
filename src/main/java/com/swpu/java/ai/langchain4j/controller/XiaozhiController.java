411c9d9c576923c30b245d2b31500df121cdc632
罗太胜 <2312763142@qq.com>
2025-09-24 17:16:03 +0800

    feat：小智智能体搭建

---
+++ src/main/java/com/swpu/java/ai/langchain4j/controller/XiaozhiController.java
@@ -0,0 +1,25 @@
+package com.swpu.java.ai.langchain4j.controller;
+
+import com.swpu.java.ai.langchain4j.assistant.XiaozhiAgent;
+import com.swpu.java.ai.langchain4j.bean.ChatForm;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "硅谷小智")
+@RestController
+@RequestMapping("/xiaozhi")
+public class XiaozhiController {
+    @Autowired
+    private XiaozhiAgent xiaozhiAgent;
+
+    @Operation(summary = "对话")
+    @PostMapping("/chat")
+    public String chat(@RequestBody ChatForm chatForm) {
+        return xiaozhiAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
+    }
+}
